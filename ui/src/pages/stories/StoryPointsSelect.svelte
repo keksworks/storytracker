44e79eaf663184070d56217c0cf35aac085e8b43
Anton Keks <anton@codeborne.com>
2024-04-24 19:41:51 +0300

    do not propagate clicks

--- ui/src/pages/stories/StoryPointsSelect.svelte
+++ ui/src/pages/stories/StoryPointsSelect.svelte
@@ -4,7 +4,8 @@
   export let points: number
 </script>
 
-<select bind:value={points} class="p-1 -m-1 border-0 bg-transparent text-blue-500 font-bold text-sm" style="background-image: none">
+<select bind:value={points} class="p-1 -m-1 border-0 bg-transparent text-blue-500 font-bold text-sm"
+        style="background-image: none" on:click|stopPropagation>
   {#each pointsLabels as label, i}
     <option value={i}>{label}</option>
   {/each}
