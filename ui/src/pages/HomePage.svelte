ffcc98ba1deb47e9053ac54e9b14ba52a01c63a4
Anton Keks <anton@codeborne.com>
2024-05-13 16:31:14 +0300

    add more benefits to hope page

--- ui/src/pages/HomePage.svelte
+++ ui/src/pages/HomePage.svelte
@@ -33,8 +33,9 @@
       <h4>These are the features we care about the most</h4>
       <ul class="list-disc list-inside">
         <li>No stories lost/hidden - everyone sees the same stories</li>
-        <li>Easy drag-based for prioritization</li>
+        <li>Easy drag-based prioritization</li>
         <li>Flow/kanban style - you just work on the next item in backlog</li>
+        <li>Simple tag-based views</li>
         <li>Velocity/estimation dates are tracked automatically</li>
         <li>In our experience, no tool can match the visibility and simplicity of this approach</li>
         <li>Suitable for truly Agile teams</li>
