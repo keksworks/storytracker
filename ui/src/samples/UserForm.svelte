1dd85400ba80a61862399f9019f196a3d208dcf6
Anton Keks <anton@azib.net>
2025-04-23 11:40:37 +0300

    drop failing UI tests

--- ui/src/samples/UserForm.svelte
+++ ui/src/samples/UserForm.svelte
@@ -7,7 +7,6 @@
   import api from 'src/api/api'
   import {showToast} from 'src/stores/toasts'
   import FormField from 'src/forms/FormField.svelte'
-  import PhoneField from 'src/forms/PhoneField.svelte'
   import RadioButton from 'src/forms/RadioButton.svelte'
 
   export let user: User = {} as User
@@ -29,8 +28,6 @@
     <FormField label={t.person.firstName} bind:value={user.firstName} autofocus={!!user.id}/>
     <FormField label={t.person.lastName} bind:value={user.lastName}/>
     <FormField label={t.contacts.email} type="email" bind:value={user.email}/>
-    <PhoneField label={t.contacts.phone} bind:value={user.phone} required={false}/>
-    <FormField label={t.person.personalCode} type="tel" bind:value={user.personalCode} required={false} minlength={11} maxlength={11} pattern="[0-6]\d\d[01]\d[0-3]\d\d\d\d\d"/>
     <RadioButton bind:value={gender} class="classic" label={t.person.gender} options={t.gender}/>
   </div>
 
