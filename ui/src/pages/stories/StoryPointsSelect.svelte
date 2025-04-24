319c3ef1419e19d043348f9f4c6a6cbe53bb41b8
Anton Keks <anton@azib.net>
2025-04-24 15:22:41 +0300

    make it more obvious that points can be changed

--- ui/src/pages/stories/StoryPointsSelect.svelte
+++ ui/src/pages/stories/StoryPointsSelect.svelte
@@ -4,7 +4,7 @@
   export let points: number | undefined
 </script>
 
-<select bind:value={points} class="p-1 -m-1 border-0 bg-transparent text-blue-500 font-bold text-sm"
+<select bind:value={points} class="p-1 -m-1 border-0 bg-transparent text-blue-500 hover:text-blue-900 font-bold text-sm cursor-pointer"
         style="background-image: none" on:click|stopPropagation>
   {#each pointsLabels as label, i}
     <option value={i}>{label}</option>
