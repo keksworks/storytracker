15ed7926730f830f812e52fe6268201f1e8f4a14
Anton Keks <anton@azib.net>
2025-04-24 17:43:16 +0300

    add status labels to action buttons

--- ui/src/pages/stories/StoryStatus.svelte
+++ ui/src/pages/stories/StoryStatus.svelte
@@ -33,22 +33,24 @@
   }
 </script>
 
-{#if story.status === StoryStatus.UNSTARTED && story.type !== StoryType.RELEASE}
-  <Button {size} color="secondary" variant="soft" on:click={start}>{t.stories.actions.start}</Button>
-{:else if story.status === StoryStatus.STARTED}
-  <Button {size} color="secondary" on:click={finish}>{t.stories.actions.finish}</Button>
-{:else if story.status === StoryStatus.DELIVERED}
-  <div class="flex gap-2">
-    <Button {size} color="success" on:click={accept}>{t.stories.actions.accept}</Button>
-    <Button {size} color="danger" on:click={reject}>{t.stories.actions.reject}</Button>
-  </div>
-{:else if story.status === StoryStatus.FINISHED}
-  <Button {size} color="warning" on:click={deliver}>{t.stories.actions.deliver}</Button>
-{:else if story.status === StoryStatus.REJECTED}
-  <Button {size} color="secondary" variant="soft" on:click={start}>
-    <div class="flex items-center space-x-2">
-      <span class="w-3 h-3 rounded-full bg-red-700"/>
-      <span>{t.stories.actions.restart}</span>
+<div title={t.stories.statuses[story.status]}>
+  {#if story.status === StoryStatus.UNSTARTED && story.type !== StoryType.RELEASE}
+    <Button {size} color="secondary" variant="soft" on:click={start}>{t.stories.actions.start}</Button>
+  {:else if story.status === StoryStatus.STARTED}
+    <Button {size} color="secondary" on:click={finish}>{t.stories.actions.finish}</Button>
+  {:else if story.status === StoryStatus.DELIVERED}
+    <div class="flex gap-2">
+      <Button {size} color="success" on:click={accept}>{t.stories.actions.accept}</Button>
+      <Button {size} color="danger" on:click={reject}>{t.stories.actions.reject}</Button>
     </div>
-  </Button>
-{/if}
+  {:else if story.status === StoryStatus.FINISHED}
+    <Button {size} color="warning" on:click={deliver}>{t.stories.actions.deliver}</Button>
+  {:else if story.status === StoryStatus.REJECTED}
+    <Button {size} color="secondary" variant="soft" on:click={start}>
+      <div class="flex items-center space-x-2">
+        <span class="w-3 h-3 rounded-full bg-red-700"/>
+        <span>{t.stories.actions.restart}</span>
+      </div>
+    </Button>
+  {/if}
+</div>
