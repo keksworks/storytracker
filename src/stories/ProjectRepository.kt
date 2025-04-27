a4cde082ee17cd9a8f4389a07057c970328f775e
Anton Keks <anton@azib.net>
2025-04-28 00:58:25 +0300

    order projects by updatedAt

--- src/stories/ProjectRepository.kt
+++ src/stories/ProjectRepository.kt
@@ -6,6 +6,8 @@ import javax.sql.DataSource
 import kotlin.reflect.KProperty1
 
 class ProjectRepository(db: DataSource): CrudRepository<Project>(db, "projects") {
+  override val defaultOrder get() = "order by updatedAt desc"
+
   fun save(project: Project, skipUpdate: Set<KProperty1<Project, *>>) =
     db.upsert(table, project.persister(), skipUpdateFields = skipUpdate.map { it.name }.toSet())
 }
