ea04bfa9604c123a8dcbe1b38b9301e62fe83e20
Anton Keks <anton@azib.net>
2025-05-21 23:58:57 +0300

    do not store tags for now

--- src/stories/ProjectRepository.kt
+++ src/stories/ProjectRepository.kt
@@ -4,12 +4,14 @@ import db.CrudRepository
 import db.Id
 import klite.jdbc.select
 import klite.jdbc.upsert
+import klite.toValuesSkipping
 import users.User
 import javax.sql.DataSource
 import kotlin.reflect.KProperty1
 
 class ProjectRepository(db: DataSource): CrudRepository<Project>(db, "projects") {
   override val defaultOrder get() = "order by updatedAt desc"
+  override fun Project.persister() = toValuesSkipping(Project::tags)
 
   fun listForMember(userId: Id<User>): List<Project> =
     db.select("$table join project_members m on $table.id = m.projectId", ProjectMember::userId to userId) { mapper() }
