3710c1f1a95c33e3363df0224b656390f5eb3c30
Anton Keks <anton@azib.net>
2025-05-21 23:13:20 +0300

    simplify code by binding the value

--- ui/src/pages/stories/StoryTagsEditor.svelte
+++ ui/src/pages/stories/StoryTagsEditor.svelte
@@ -6,28 +6,28 @@
   export let story: Story
 
   let focused = false
+  let value = ''
 
   function remove(i: number) {
     story.tags.splice(i, 1)
     story = story
   }
 
-  function add(e: Event) {
-    const input = e.currentTarget as HTMLInputElement
-    const value = input.value.trim()
+  function add() {
+    value = value.trim()
     if (value) {
       story.tags.push(value)
       story = story
-      input.value = ''
       if (!project.tags.includes(value)) project.tags.push(value)
+      value = ''
     }
   }
 
   function keydown(e: KeyboardEvent) {
     if (e.key === 'Enter') {
       e.preventDefault()
-      add(e)
-    } else if (e.key === 'Backspace' && !e.currentTarget!['value']) {
+      add()
+    } else if (e.key === 'Backspace' && !value) {
       e.preventDefault()
       remove(story.tags.length - 1)
     }
@@ -38,7 +38,7 @@
   {#each story.tags as tag, i}
     <Badge class="success whitespace-nowrap !py-0">{tag} <button class="ml-1 py-0.5" on:click={() => remove(i)}>✕</button></Badge>
   {/each}
-  <input class="w-32 flex-1 border-none py-0.5 pl-1 focus:outline-none focus:ring-0" on:change={add} on:keydown={keydown} on:focus={() => focused = true} on:blur={() => focused = false} list="tags-{story.id}">
+  <input class="w-32 flex-1 border-none py-0.5 pl-1 focus:outline-none focus:ring-0" bind:value on:change={add} on:keydown={keydown} on:focus={() => focused = true} on:blur={() => focused = false} list="tags-{story.id}">
   {#if focused}
     <datalist id="tags-{story.id}">
       {#each project.tags.filter(t => !story.tags.includes(t)) as tag}<option value={tag}>{/each}
