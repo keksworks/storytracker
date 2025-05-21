bccbdf59e308d84efb80cf1a52a6c39be0b468eb
Anton Keks <anton@azib.net>
2025-05-21 22:36:43 +0300

    make more advanced adding of tags

--- ui/src/pages/stories/StoryTagsEditor.svelte
+++ ui/src/pages/stories/StoryTagsEditor.svelte
@@ -1,10 +1,35 @@
 <script lang="ts">
-  import FormField from 'src/forms/FormField.svelte'
   import type {Story} from 'src/api/types'
+  import Badge from 'src/components/Badge.svelte'
 
   export let story: Story
-  let tags = story.tags.join(', ')
-  $: story.tags = tags.split(',').map(t => t.trim()).filter(t => t)
+
+  function remove(i: number) {
+    story.tags.splice(i, 1)
+    story = story
+  }
+
+  function add(e: Event) {
+    const input = e.target as HTMLInputElement
+    const value = input.value.trim()
+    if (value) {
+      story.tags.push(value)
+      story = story
+      input.value = ''
+    }
+  }
+
+  function keydown(e: KeyboardEvent) {
+    if (e.key === 'Enter') {
+      e.preventDefault()
+      add(e)
+    }
+  }
 </script>
 
-<FormField bind:value={tags} on:keydown placeholder="Comma-separated for now"/>
+<div class="border rounded bg-white flex items-center gap-1" class:pl-1={story.tags.length}>
+  {#each story.tags as tag, i}
+    <Badge class="success">{tag} <button class="ml-1 py-0.5" on:click={() => remove(i)}>✕</button></Badge>
+  {/each}
+  <input class="flex-1 border-none rounded" on:blur={add} on:keydown={keydown}>
+</div>
