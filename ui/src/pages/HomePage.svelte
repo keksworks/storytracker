a2fa198239d7ace37c79fb14add26e428386acc2
Anton Keks <anton@azib.net>
2025-05-21 22:09:58 +0300

    add some info texts for the public home page

--- ui/src/pages/HomePage.svelte
+++ ui/src/pages/HomePage.svelte
@@ -5,12 +5,13 @@
 
 <MainPageLayout title={t.title}>
   <section class="grid gap-10 p-6 sm:p-12">
+    <h2>Inspired by PivotalTracker - Agile project tracker done right</h2>
     <div class="grid gap-4">
-      <h2>Features</h2>
+      <h3>Features</h3>
       <ul class="list-disc list-inside">
         <li>No stories lost/hidden - everyone sees the same stories</li>
         <li>Easy drag-based prioritization</li>
-        <li>Flow/kanban style - you just work on the next item in backlog</li>
+        <li>Flow/Kanban style - you just work on the next item in backlog</li>
         <li>Simple tag-based views</li>
         <li>Velocity/estimation dates are tracked automatically</li>
         <li>In our experience, no tool can match the visibility and simplicity of this approach</li>
