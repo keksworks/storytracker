5b18d2c0e11abebc16686a92221db4e109a2854a
Anton Keks <anton@azib.net>
2025-09-12 12:33:35 +0300

    translate all messages to Estonian

--- ui/src/i18n.ts
+++ ui/src/i18n.ts
@@ -14,7 +14,6 @@ export async function initTranslations(opts?: Partial<Options>) {
   if (lang) rememberLang(lang)
   else lang = detectLang(() => navigator.languages?.find(l => l.startsWith('et')) ? 'et' : undefined)
   const promise = lang == 'et' ? import('i18n/et.json') :
-    lang == 'ru' ? import('i18n/ru.json') :
       import('i18n/en.json')
   let dict = await promise as unknown as typeof enDict
   if (import.meta.env.DEV && lang != defaultLang) {
