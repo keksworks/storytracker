11b59ad635921688465be2e5ab38b6be11a97be8
Anton Keks <anton@azib.net>
2025-11-27 22:21:38 +0200

    update logs

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 mkdir -p logs
-docker logs aitavalida-aitavalida-1 2>&1 | gzip > logs/aitavalida-"$(date +%Y-%m-%d_%H:%m:%S)".log.gz
+docker compose logs -t tracker 2>&1 | gzip > logs/tracker-"$(date +%Y-%m-%d_%H:%m:%S)".log.gz
 docker compose -f docker-compose.yml -f docker-compose.deploy.yml --env-file .env --env-file .env.prod up -d --build
