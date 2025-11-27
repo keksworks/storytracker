c228f7dde090e37755624ab6e20ba3e41bb4640b
Anton Keks <anton@azib.net>
2025-11-27 21:11:52 +0200

    use floats on mobile screens

--- ui/src/pages/stories/StoryView.svelte
+++ ui/src/pages/stories/StoryView.svelte
@@ -86,8 +86,8 @@
      on:dragover|preventDefault={() => isDropTarget = true} on:dragleave={() => isDropTarget = false} class:drop-target={isDropTarget}
 >
   <!--svelte-ignore a11y-click-events-have-key-events -->
-  <div class="flex justify-between items-center gap-x-2 gap-y-0.5 px-3 py-2" on:click={() => open = !open}>
-    <span title={t.stories.types[story.type]}>
+  <div class="sm:flex justify-between items-center gap-x-2 gap-y-0.5 px-3 py-2" on:click={() => open = !open}>
+    <span title={t.stories.types[story.type]} class="max-sm:float-left mr-1">
       {#if story.type == StoryType.FEATURE}
         <Icon name="star-filled" class="text-yellow-500"/>
       {:else if story.type == StoryType.CHORE}
@@ -116,7 +116,8 @@
         </ul>
       {/if}
     </div>
-    <div class="flex items-center gap-3">
+
+    <div class="flex items-center gap-3 max-sm:float-right">
       <StoryActions {story} {save} {open}/>
       <StoryPointsSelect bind:points={story.points} onchange={() => save()} disabled={!!story.acceptedAt}/>
       <Icon name={open ? 'chevron-up' : 'chevron-down'} class="cursor-pointer"/>
