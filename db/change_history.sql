ba9ae33bd0c63301187f91c851e03ca17d7357a8
Anton Keks <anton@azib.net>
2026-03-11 23:39:07 +0200

    store projectId in change_history

--- db/change_history.sql
+++ db/change_history.sql
@@ -9,6 +9,9 @@ create table change_history(
   changedBy bigint
 );
 
+--changeset change_history.projectId
+alter table change_history add column projectId bigint;
+
 --changeset change_history.idx
 create index change_history_id_idx on change_history("table", rowId);
 
@@ -33,21 +36,27 @@ end; $$
 --changeset add_change_history onChange:RUN separator:none
 create or replace function add_change_history() returns trigger language plpgsql as $$
 declare
-  col name;
+  col text;
   oldValue text;
   newValue text;
+  newRec jsonb := to_jsonb(new);
+  oldRec jsonb := to_jsonb(old);
+  pid bigint := case when tg_table_name = 'projects' then new.id else (newRec->>'projectid')::bigint end;
 begin
-  for col in (
-    select attname from pg_catalog.pg_attribute a
-      join pg_catalog.pg_class c on c.oid = a.attrelid and c.relname = tg_table_name
-      join pg_catalog.pg_namespace n on n.oid = c.relnamespace and n.nspname = tg_table_schema
-    where a.attnum > 0 and not a.attisdropped)
-    loop
-      execute format('select (($1).%I)::text', col) using new into newValue;
-      execute format('select (($1).%I)::text', col) using old into oldValue;
-      if (oldValue is not distinct from newValue or col in ('updatedat')) then continue; end if;
-
-      insert into change_history ("table", rowId, "column", oldValue, newValue, changedBy) values (tg_table_name, new.id, col, oldValue, newValue, get_app_user());
-    end loop;
+  for col, newValue in select * from jsonb_each_text(newRec) loop
+    if (col not in ('updatedat', 'projectid', 'id')) then
+      oldValue := oldRec->>col;
+      if (oldValue is distinct from newValue) then
+        insert into change_history ("table", rowId, "column", oldValue, newValue, changedBy, projectId)
+        values (tg_table_name, new.id, col, oldValue, newValue, get_app_user(), pid);
+      end if;
+    end if;
+  end loop;
   return new;
 end; $$
+
+--changeset change_history.projectId:fill
+update change_history h set projectId = (select projectId from stories s where s.id::text = h.rowId) where "table" = 'stories' and projectId is null;
+update change_history h set projectId = (select projectId from epics e where e.id::text = h.rowId) where "table" = 'epics' and projectId is null;
+update change_history h set projectId = (select projectId from project_members m where m.id::text = h.rowId) where "table" = 'project_members' and projectId is null;
+update change_history h set projectId = rowId::bigint where "table" = 'projects' and projectId is null;
