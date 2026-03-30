5beb7689e78ba6967c5cfcf40abd3fff10dbde3c
Anton Keks <anton@azib.net>
2026-03-30 16:17:34 +0300

    always show logo

--- ui/src/layout/Header.svelte
+++ ui/src/layout/Header.svelte
@@ -8,11 +8,12 @@
 </script>
 
 <header class="bg-stone-50 border-b px-4 py-3 flex flex-wrap gap-3 justify-between items-center">
-  {#if title}
-    <h1 class="font-bold text-2xl mr-4">{title}</h1>
-  {:else}
-    <div class="sm:w-60"><Logo/></div>
-  {/if}
+  <div class="flex gap-2.5 items-center">
+    <Logo/>
+    {#if title}
+      <h1 class="font-bold text-2xl mr-4">{title}</h1>
+    {/if}
+  </div>
 
   <div class="mr-auto">
     <Navigation/>
